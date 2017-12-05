2f2ba923e9a3b4a760d3ac05b63e1dcc8785933c
zhangli <342412055@qq.com>
2017-12-05 16:38:16 +0800

    RoundLabelTextView

--- app/src/main/java/com/github/namelesspeople/roundlabelview1/MainActivity.java
+++ app/src/main/java/com/github/namelesspeople/roundlabelview1/MainActivity.java
@@ -4,13 +4,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.github.namelesspeople.roundlabeltextview.RoundLabelTextView;
 
 public class MainActivity extends AppCompatActivity {
     RoundLabelTextView textView;
-    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
